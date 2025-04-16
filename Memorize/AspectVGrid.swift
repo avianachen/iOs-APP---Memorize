2816dbe33d74f588e0a351d210a22406445e5ad8
avianachen <17806790352@163.com>
2025-04-16 14:40:38 +0800

    创建自定义的能够调整纵横比的网格视图/布局

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by cxq on 2025/4/16.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
